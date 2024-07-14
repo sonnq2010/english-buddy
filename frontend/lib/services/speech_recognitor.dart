87d281ebe4e194a2877dae0b8b072c7b5ed55d54
Son Nguyen <sonnq2010@gmail.com>
2024-07-14 19:09:52 +0700

    #22 Continue implement

--- frontend/lib/services/speech_recognitor.dart
+++ frontend/lib/services/speech_recognitor.dart
@@ -1,5 +1,8 @@
 import 'dart:developer';
 
+import 'package:frontend/models/web_socket_message.dart';
+import 'package:frontend/services/cc_service.dart';
+import 'package:frontend/services/web_socket_service.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
@@ -16,24 +19,27 @@ class SpeechRecognitor {
       onStatus: (status) {
         log(status.toString());
         if (status == 'notListening') {
-          startListen();
+          startListen(clearCC: false);
         }
       },
       onError: (error) => log(error.errorMsg),
     );
   }
 
-  Future<void> startListen() async {
+  Future<void> startListen({bool clearCC = true}) async {
     _speech.listen(
       onResult: _onResult,
       listenOptions: stt.SpeechListenOptions(
         partialResults: true,
       ),
     );
+
+    if (clearCC) CCService.I.clearCC();
   }
 
   Future<void> stopListen() async {
     _speech.stop();
+    CCService.I.clearCC();
   }
 
   void _onResult(SpeechRecognitionResult recognitionResult) {
@@ -41,8 +47,8 @@ class SpeechRecognitor {
     if (result == _lastResult) return;
 
     final minifiedResult = minifyRecognizedWords(result);
-    log(minifiedResult);
-    // TODO: Send to socket
+    final message = WebSocketMessage.cc(minifiedResult);
+    WebSocketService.I.sendMessage(message);
 
     _lastResult = result;
   }
